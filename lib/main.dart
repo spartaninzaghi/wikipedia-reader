550aa8485e39e6abeed2da6849eca2c4accb751b
Mate Narh <spartaninzaghi@gmail.com>
2026-02-16 20:25:18 -0500

    explored the Model-View-ViewModel (MVVM) architecture pattern, built an HTTP request to fetch Wikipedia data, and handled error while parsing JSON responses

--- lib/main.dart
+++ lib/main.dart
@@ -27,3 +27,21 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
+
+class ArticleModel {
+  Future<Summary> getRandomArticleSummary() async {
+    final uri = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/random/summary',
+    );
+    final response = await get(uri);
+
+    if (response.statusCode != 200) {
+      throw HttpException('Failed to update resource');
+    }
+
+    return Summary.fromJson(jsonDecode(response.body));
+  }
+}
+
