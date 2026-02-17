e9701e3efae37af8bb8ce7ec7d64df3656cd8ab4
Mate Narh <spartaninzaghi@gmail.com>
2026-02-17 02:03:39 -0500

    used ListenableBuilder to rebuild UI automatically, handled all possible app states with switch expressions, built the complete View layer with proper styling, hence, effectively learning and applying the MVVM architecture

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    final viewModel = ArticleViewModel(ArticleModel());
-    
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Wikipedia Flutter'),
-        ),
-        body: const Center(
-          child: Text('Loading...'),
-        ),
-      ),
+      home: ArticleView(),
     );
   }
 }
@@ -63,14 +53,117 @@ class ArticleViewModel extends ChangeNotifier {
     notifyListeners();
     try {
       summary = await model.getRandomArticleSummary();
-      print('Article loaded: ${summary!.titles.normalized}'); // Temporary
+      // print('Article loaded: ${summary!.titles.normalized}'); // Temporary
       errorMessage = null; // Clear any previous errors.
     } on HttpException catch (error) {
-      print('Error loading article: ${error.message}'); // Temporary
+      // print('Error loading article: ${error.message}'); // Temporary
       errorMessage = error.message;
       summary = null;
     }
     loading = false;
     notifyListeners();
+  }
+}
+
+class ArticleView extends StatelessWidget {
+  ArticleView({super.key});
+
+  final ArticleViewModel viewModel = ArticleViewModel(ArticleModel());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wikipedia Flutter'),
+        actions: [],
+      ),
+      body: ListenableBuilder(
+        listenable: viewModel,
+        builder: (context, child) {
+          return switch ((
+            viewModel.loading,
+            viewModel.summary,
+            viewModel.errorMessage,
+          )) {
+            (true, _, _) => CircularProgressIndicator(),
+            (false, _, String message) => Center(child: Text(message)),
+            (false, null, null) => Center(
+              child: Text('An unknown error has occurred'),
+            ),
+            // The summary must be non-null in this switch case.
+            (false, Summary summary, null) => ArticlePage(
+              summary: summary,
+              nextArticleCallback: viewModel.getRandomArticleSummary,
+            ),
+          };
+        },
+      ),
+    );
+  }
+}
+
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({
+    super.key,
+    required this.summary,
+    required this.nextArticleCallback,
+  });
+
+  final Summary summary;
+  final VoidCallback nextArticleCallback;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ArticleWidget(
+            summary: summary,
+          ),
+          ElevatedButton(
+            onPressed: nextArticleCallback,
+            child: Text('Next random article'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class ArticleWidget extends StatelessWidget {
+  const ArticleWidget({super.key, required this.summary});
+
+  final Summary summary;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 10.0,
+        children: [
+          if (summary.hasImage)
+            Image.network(
+              summary.originalImage!.source,
+            ),
+          Text(
+            summary.titles.normalized,
+            overflow: TextOverflow.ellipsis,
+            style: TextTheme.of(context).displaySmall,
+          ),
+          if (summary.description != null)
+            Text(
+              summary.description!,
+              overflow: TextOverflow.ellipsis,
+              style: TextTheme.of(context).bodySmall,
+            ),
+          Text(
+            summary.extract,
+          ),
+        ],
+      ),
+    );
   }
 }
